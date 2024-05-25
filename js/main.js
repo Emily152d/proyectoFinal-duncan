79019ad6d1c32bfb68215308b46b439cca1dab26
Emily Duncan <mimi@MacBook-Air-de-Emily.local>
2024-05-24 22:02:21 -0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+let cant_notas = Number(prompt('Ingrese la cantidad de notas a promediar'));
+
+let suma_notas = 0;
+
+for (let i = 1; i <= cant_notas; i++) {
+    let nota = Number(prompt('Ingrese la nota ' + i));
+    
+    while (isNaN(nota) || nota <= 0 || nota >= 11 || nota === '') {
+        nota = Number(prompt('Ingrese la nota ' + i + ' correctamente (entre 1 y 10):'));
+    }
+    
+    suma_notas += nota;
+}
+
+let promedio = suma_notas / cant_notas;
+
+console.log('El promedio es de', promedio);
+
+if (promedio > 6) {
+    console.log('Aprueba');
+} else {
+    console.log('No aprueba');
+}
